5bc96081244ed2cca1ccabc83191083229db5733
Carlos Pliego <carlos.oogs@gmail.com>
2014-10-24 18:33:51 -0600

    create user is now working.

--- www/js/controllers/signup.js
+++ www/js/controllers/signup.js
@@ -1,8 +1,17 @@
 'use strict';
 
-app.controller('SignupCtrl', function ($scope) {
+app.controller('SignupCtrl', function ($scope, User) {
 
-    $scope.signup = function(){
-        log("Signing up");
+    $scope.signup = function () {
+        // Remove confirm_password from object.
+        var user = angular.copy($scope.user);
+        delete user.confirm_password;
+
+        // Save
+        User.fill(user);
+        User.create().then(function () {
+            log("user was just created");
+            // Log user in.
+        });
     }
 });
