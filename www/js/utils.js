72862fdd969a4806779881cf7ba4511eb508f586
Carlos Pliego <carlos.oogs@gmail.com>
2014-10-24 20:21:52 -0600

    Removed unused comments and added a watch to the karma tests

--- www/js/utils.js
+++ www/js/utils.js
@@ -4,26 +4,3 @@ function log() {
         console.log.apply(console, arguments);
     }
 }
-/*
- *
- *
- function generateUUID(){
- var d = new Date().getTime();
- var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
- var r = (d + Math.random()*16)%16 | 0;
- d = Math.floor(d/16);
- return (c=='x' ? r : (r&0x7|0x8)).toString(16);
- });
- return uuid;
- };
-
- // Status evaluator
- var Status = new function() {
- this.ok = function (status) {
- return (status === 200) ? true : false;
- };
- };
-
- *
- *
- * */
