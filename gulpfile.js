cbc009b9aff020372fea3dc65317a2f4a587fe9f
Carlos Pliego <carlos.oogs@gmail.com>
2014-11-05 14:22:20 -0700

    more gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -22,59 +22,53 @@ gulp.task('buildR-ios', function () {
 });
 
 gulp.task('build', function () {
-    sh.exec("rm -r ../dist/");
-    sh.exec("mkdir ../dist");
+    sh.exec("rm -r dist/");
+    sh.exec("mkdir dist");
     //sh.exec("mkdir dist && mkdir dist/lib && mkdir dist/core/dist");
-    sh.cp('-R', 'lib/', '../dist/lib/');
+    sh.cp('-R', 'app/lib/', 'dist/lib/');
     sh.exec("gulp scripts");
     sh.exec("gulp scss");
-    sh.cp('-R', 'img/*', '../dist/img');
-    sh.cp('-R', 'views/*', '../dist/views');
+    sh.cp('-R', 'app/img/*', 'dist/img');
+    sh.cp('-R', 'app/views/*', 'dist/views');
     sh.exec("gulp index");
-    sh.cp('cordova.js', '../dist/cordova.js');
+    sh.cp('app/cordova.js', 'dist/cordova.js');
 });
 
 
 gulp.task('scripts', function () {
     gulp.src([
-        'js/utils.js',
-        'js/app.js',
-        'js/constants.js',
-        'js/routes.js',
-        'js/controllers/home.js',
-        'js/controllers/login.js',
-        'js/controllers/signup.js',
-        'js/services/user.js'
+        'app/js/utils.js',
+        'app/js/app.js',
+        'app/js/constants.js',
+        'app/js/routes.js',
+        'app/js/controllers/home.js',
+        'app/js/controllers/login.js',
+        'app/js/controllers/signup.js',
+        'app/js/services/user.js'
     ])
         .pipe(concat('scripts.js'))
         //.pipe(uglify())
-        .pipe(gulp.dest('../dist/js'));
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('scss', function () {
-    gulp.src('scss/main.scss')
+    gulp.src('app/scss/main.scss')
         .pipe(sass())
         .pipe(minifyCss({
             keepSpecialComments: 0
         }))
         .pipe(rename({extname: '.min.css'}))
-        .pipe(gulp.dest('../dist/css'));
+        .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('index', function () {
-    sh.cp('index.html', '../dist/index.html');
-    gulp.src(['../dist/index.html'])
+    sh.cp('app/index.html', 'dist/index.html');
+    gulp.src(['dist/index.html'])
         .pipe(replace(/<!--saijs:scripts:dev-->((\n)(.)*)+<!--!saijs:scripts:dev-->/, ''))
         .pipe(replace('saijs:scripts:build', ''))
         .pipe(replace('<!--', ''))
         .pipe(replace('-->', ''))
-        .pipe(gulp.dest('../dist/'));
-});
-
-gulp.task('install', function () {
-    sh.exec("echo 'brains'");
-    // npm install for server/package.json
-    // npm install for package.json
+        .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('db-reset', function () {
--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
     config.set({
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: 'www/system),
+        basePath: 'www/system)',
 
 
         // frameworks to use
